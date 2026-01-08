a14c2c16cf265436ff908ef4a3c54f2e0712ec2f
Makar <kolesnykmakar0@gmail.com>
2026-01-08 07:30:36 +0200

    Add gallery & HTMLCollection

--- index.js
+++ index.js
@@ -1,36 +1,9 @@
-const h1 = document.getElementById("main-tittle");
-// h1.style.textAlign = 'center'
-// h1.style.color = 'red'
-// h1.style.backgroundColor = 'yellow'
-// h1.textContent= 'hi from javascript'
+const bigImg = document.getElementById('big')
 
-h1.className = "heading";
+const smallImgs = document.getElementsByClassName('small')
 
-h1.textContent = "hi from javascript";
-h1.title = "tittle for main tittle";
-
-console.log(h1);
-console.dir(h1);
-
-const p = document.getElementById("text");
-
-p.style.fontSize = "24px";
-p.textContent = "have a nice day";
-
-const pic = (document.getElementById = "img");
-console.dir(img);
-console.log(img);
-
-img.src =
-  "https://www.woodchipandmagnolia.co.uk/cdn/shop/files/WM-457-01-EBBANDFLOW-AQUA-03.jpg?v=1715248326&width=128";
-img.alt = "sea";
-img.width = 100;
-img.height = 100;
-img.style.objectFit = "cover";
-
-const btn = document.getElementById("btn");
-console.dir(btn);
-
-btn.style.height = "70px";
-btn.style.width = "200px";
-btn.textContent += ", please";
+for (const smallImg of smallImgs) {
+  smallImg.addEventListener('click', ()=>{
+    bigImg.src = smallImg.src
+  })
+}
