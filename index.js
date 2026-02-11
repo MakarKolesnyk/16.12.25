0a7698bb10daae15f3d9399b72862d8cd05e510f
Makar <kolesnykmakar0@gmail.com>
2026-02-11 03:46:28 +0200

    add stopPropagation

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const wrapper = document.getElementById("wrapper");
 const btn = document.getElementById("btn");
 wrapper.addEventListener("click", (event) => {
-  console.log('click wrapper');
-  // console.group();
-  // console.log(event);
-  // console.log(event.target);
-  // console.log(event.currentTarget);
-  // console.group()
-  // console.log(event.clientX, event.clientX);
-  // console.log(event.offsetXX, event.offsetXX);
-  // console.groupEnd();
+  console.log("click wrapper");
+  event.target.style.color = "red";
 });
-btn.addEventListener('click', ()=>{
-  console.log('click btn');
-})
+btn.addEventListener("click", (event) => {
+  event.stopPropagation();
+  console.log("click btn");
+});
