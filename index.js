8c3a726eeaa7f8e8228ca2202de64dff18d4274f
Makar <kolesnykmakar0@gmail.com>
2026-02-11 02:49:59 +0200

    Add classList

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const tasks = document.getElementsByClassName('task')
 
-for (const task of tasks) {
-  task.addEventListener('click', ()=>{
-  if (confirm('Do you want to delete?')) {
-      task.style.display = 'none'
-  }
-  })
-}
+const btns = document.querySelectorAll('button')
 
-const btns = document.getElementsByClassName('btn')
-const block = document.getElementById('block')
+// classList
 
 for (const btn of btns) {
-  btn.style.backgroundColor = btn.textContent;
-  btn.addEventListener('click', ()=>{
-    block.style.backgroundColor = btn.textContent;
+  // console.dir(btn.classList)
+  btn.classList.add('btn');
+  btn.addEventListener('mouseenter', ()=>{
+    btn.classList.add('active')
+  })
+  btn.addEventListener('mouseout', ()=>{
+    btn.classList.remove('active')
   })
 }
