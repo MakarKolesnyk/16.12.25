8d680534b5820ee92e279e9bbbdd02ccc3d580d4
Makar <kolesnykmakar0@gmail.com>
2026-02-11 03:39:44 +0200

    Add event

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const btn = document.querySelector('#block > button')
-console.dir(btn)
-
-const img = document.querySelector('#block > img')
-
+const wrapper = document.getElementById("wrapper");
+const btn = document.getElementById("btn");
+wrapper.addEventListener("click", (event) => {
+  console.log('click wrapper');
+  // console.group();
+  // console.log(event);
+  // console.log(event.target);
+  // console.log(event.currentTarget);
+  // console.group()
+  // console.log(event.clientX, event.clientX);
+  // console.log(event.offsetXX, event.offsetXX);
+  // console.groupEnd();
+});
 btn.addEventListener('click', ()=>{
-  img.classList.toggle('hide')
-  btn.textContent=btn.textContent==='show picture'?'hide picture' : 'show picture'
-})
-
-const btnTheme = document.querySelector('button:first-of-type')
-btnTheme.addEventListener('click', ()=>{
-  btnTheme.textContent = btnTheme.textContent === 'light'?'dark':'light'
-  document.body.classList.toggle('dark')
+  console.log('click btn');
 })
